2c168d92122e7ad90fbe1e221e2df1314c5ee44d
JohnTheRobot <Johntherobot5000@gmail.com>
2019-02-07 18:23:44 -0500

    Declare gamePieceManipulatorJake in the GamePlay class

--- src/main/java/org/livoniawarriors/Robot2019/subsystems/gameplay/GamePlay.java
+++ src/main/java/org/livoniawarriors/Robot2019/subsystems/gameplay/GamePlay.java
@@ -5,16 +5,18 @@ import org.livoniawarriors.Robot2019.ISubsystem;
 public class GamePlay implements ISubsystem {
 
     private Elevator elevator;
+    private GamePieceManipulatorJake gamePieceManipulatorJake;
 
     @Override
     public void init() {
         elevator = new Elevator();
+        gamePieceManipulatorJake = new GamePieceManipulatorJake();
     }
 
     @Override
     public void update(boolean enabled) {
         elevator.update(enabled);
-
+        gamePieceManipulatorJake.update(enabled);
     }
 
     @Override
