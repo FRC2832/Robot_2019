28fd7d587de6c5b1e2e2d7103eff105760f8c7a9
JohnTheRobot <Johntherobot5000@gmail.com>
2019-02-07 19:09:13 -0500

    Final Merge commit, I hope

--- src/main/java/org/livoniawarriors/Robot2019/subsystems/gameplay/GamePlay.java
+++ src/main/java/org/livoniawarriors/Robot2019/subsystems/gameplay/GamePlay.java
@@ -1,26 +1,32 @@
 package org.livoniawarriors.Robot2019.subsystems.gameplay;
 
 import org.livoniawarriors.Robot2019.ISubsystem;
+import org.livoniawarriors.Robot2019.subsystems.diagnostic.IDiagnosable;
 
-public class GamePlay implements ISubsystem {
+public class GamePlay implements ISubsystem, IDiagnosable {
 
     private Elevator elevator;
     private GamePieceManipulatorJake gamePieceManipulatorJake;
 
-    @Override
-    public void init() {
+	@Override
+	public void init() {
         elevator = new Elevator();
         gamePieceManipulatorJake = new GamePieceManipulatorJake();
-    }
+	}
 
-    @Override
-    public void update(boolean enabled) {
+	@Override
+	public void update(boolean enabled) {
         elevator.update(enabled);
         gamePieceManipulatorJake.update(enabled);
-    }
+	}
 
-    @Override
-    public void dispose() throws Exception {
+	@Override
+	public void dispose() throws Exception {
 
-    }
+	}
+
+	@Override
+	public void diagnose() {
+		elevator.diagnose();
+	}
 }
