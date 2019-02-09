2f73615e0c68181698c3fe707335436bf96d2da0
LogicMaster63 <justinmarentette11@gmail.com>
2019-02-09 11:56:09 -0500

    Fixed broken import

--- src/main/java/org/livoniawarriors/Robot2019/subsystems/gameplay/GamePlay.java
+++ src/main/java/org/livoniawarriors/Robot2019/subsystems/gameplay/GamePlay.java
@@ -2,7 +2,6 @@ package org.livoniawarriors.Robot2019.subsystems.gameplay;
 
 import org.livoniawarriors.Robot2019.ICsvLogger;
 import org.livoniawarriors.Robot2019.ISubsystem;
-import org.livoniawarriors.Robot2019.subsystems.diagnostic.IDiagnosable;
 
 public class GamePlay implements ISubsystem {
 
