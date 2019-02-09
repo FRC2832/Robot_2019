a1725ed87375bca4a3dea660ffe0c2f20d71a692
Dillan Pribak <dmpribak@gmail.com>
2019-02-08 19:42:52 -0500

    Pressure Sensor

--- src/main/java/org/livoniawarriors/Robot2019/subsystems/PeripheralSubsystem.java
+++ src/main/java/org/livoniawarriors/Robot2019/subsystems/PeripheralSubsystem.java
@@ -1,6 +1,8 @@
 package org.livoniawarriors.Robot2019.subsystems;
 
 import org.livoniawarriors.Robot2019.Lidar;
+
+import edu.wpi.first.wpilibj.AnalogInput;
 import org.livoniawarriors.Robot2019.ISubsystem;
 
 import java.io.IOException;
@@ -8,19 +10,27 @@ import java.io.IOException;
 public class PeripheralSubsystem implements ISubsystem {
 
     private Lidar lidar;
+    private AnalogInput pressureSensor;
+    private double pressure;
 
     @Override
     public void init() {
         lidar = new Lidar();
+        pressureSensor = new AnalogInput(1);
     }
 
     @Override
     public void update(boolean enabled) {
         lidar.update();
+        pressure = 250d * (double)pressureSensor.getVoltage() / 5d - 25d;
     }
 
     @Override
     public void dispose() throws IOException {
         lidar.dispose();
+    }
+
+    public double getPressure() {
+        return pressure;
     }
 }
