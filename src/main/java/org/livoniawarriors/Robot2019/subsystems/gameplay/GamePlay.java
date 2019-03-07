5af3116c8eaa3be1caa85c65179f55a879743ea4
JohnTheRobot <Johntherobot5000@gmail.com>
2019-03-07 18:34:45 -0500

    Added Temporary 6 inch climber

--- src/main/java/org/livoniawarriors/Robot2019/subsystems/gameplay/GamePlay.java
+++ src/main/java/org/livoniawarriors/Robot2019/subsystems/gameplay/GamePlay.java
@@ -1,26 +1,47 @@
 package org.livoniawarriors.Robot2019.subsystems.gameplay;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 import org.livoniawarriors.Robot2019.ICsvLogger;
 import org.livoniawarriors.Robot2019.ISubsystem;
+import org.livoniawarriors.Robot2019.Robot;
+import org.livoniawarriors.Robot2019.UserInput;
+
+import edu.wpi.first.wpilibj.Talon;
 
 public class GamePlay implements ISubsystem {
 
 	private Elevator elevator;
 	private GamePieceManipulator gamePieceManipulator;
-    private Climber climber;
+    //private Climber climber;
+
+    //Climber motors
+    private Talon climberLeft, climberRight;
 
 	@Override
 	public void init() {
         elevator = new Elevator();
 		gamePieceManipulator = new GamePieceManipulator();
-        climber = new Climber();
+        //climber = new Climber();
+        climberLeft = new Talon(0);
+        climberRight = new Talon(1);
 	}
 
 	@Override
 	public void update(boolean enabled) {	
 		gamePieceManipulator.update(enabled);
-        climber.update(enabled);
+        //climber.update(enabled);
         elevator.update(enabled);
+
+        //Climber
+        if (Robot.userInput.getController(UserInput.Controllers.XBOX).getButton(UserInput.Button.BUMPER_L)) {
+            climberLeft.set(0.7);
+            climberRight.set(0.7);
+        } else if (Robot.userInput.getController(UserInput.Controllers.XBOX).getButton(UserInput.Button.BUMPER_L)) {
+            climberLeft.set(-0.7);
+            climberRight.set(-0.7);
+        }
+            
 	}
 	
 	public double getElevatorHeight() {
