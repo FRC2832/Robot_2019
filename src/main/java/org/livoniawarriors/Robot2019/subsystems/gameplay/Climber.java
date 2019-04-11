8359887dbfeaa7f814b7959101489086a36847a8
JohnTheRobot <Johntherobot5000@gmail.com>
2019-04-11 10:03:24 -0400

    Updates to climber
    This includes adding the 2 talons that control the back wood thing and
    updating the controls to what the drive team wants.  (Also added buttons
    0, 1, and 2 to user input because they weren't there and those are the
    buttons we want)

--- src/main/java/org/livoniawarriors/Robot2019/subsystems/gameplay/Climber.java
+++ src/main/java/org/livoniawarriors/Robot2019/subsystems/gameplay/Climber.java
@@ -6,7 +6,6 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import org.apache.logging.log4j.Level;
 import org.livoniawarriors.Robot2019.Robot;
 import org.livoniawarriors.Robot2019.UserInput;
 import org.livoniawarriors.Robot2019.UserInput.Button;
@@ -17,52 +16,53 @@ import org.livoniawarriors.Robot2019.UserInput.Joystick;
 
 public class Climber {
 
-	private static final int CLIMBER_FRONT = 23;
-	private static final int CLIMBER_BACK = 12;
-	private CANSparkMax climberFront;
-	private WPI_TalonSRX climberBack;
-	private UserInput.Controller controller;
+    private static final int CLIMBER = 23;
+    private static final int LEFT_PUSHER = 17; //TODO: Set to real numbers
+    private static final int RIGHT_PUSHER = 18;
+    private CANSparkMax climber;
+    private WPI_TalonSRX leftPusher, rightPusher;
+	private UserInput.Controller leftFlightStick, rightFlightStick;
 
 	public Climber() {
-		climberFront = new CANSparkMax(CLIMBER_FRONT, MotorType.kBrushless);
-		climberBack = new WPI_TalonSRX(CLIMBER_BACK);
-		controller = Robot.userInput.getController(Controllers.XBOX);
+        climber = new CANSparkMax(CLIMBER, MotorType.kBrushless);
+        leftPusher = new WPI_TalonSRX(LEFT_PUSHER);
+        rightPusher = new WPI_TalonSRX(RIGHT_PUSHER);
+        rightPusher.setInverted(true);
+        
+	    leftFlightStick = Robot.userInput.getController(Controllers.L_FLIGHTSTICK);
+        rightFlightStick = Robot.userInput.getController(Controllers.R_FLIGHTSTICK);
+        
 	}
 
 	public void update(boolean enabled) {
-		if(!enabled) 
-			return;
+		if(!enabled) {
+            return;
+        }
 		
-		moveFrontClimber();
-		moveBackClimber();
-		
-	}
+		if(rightFlightStick.getButton(Button.THREE)) {
+			climber.set(-0.8);
+        } else if (rightFlightStick.getButton(Button.FOUR)) {
+            climber.set(0.8);
+        } else {
+            climber.set(0);
+        }
 
-	private void moveFrontClimber() {
-		//Move climber arm down
-		if(controller.getButton(Button.BUMPER_R)) {
-			climberFront.set(-0.5);
-		}
-		//Move climber arm up
-		else if(controller.getButton(Button.BUMPER_L)) {
-			climberFront.set(0.5);
+        if (leftFlightStick.getButton(Button.ONE)) {
+            leftPusher.set(0.7);
+        } else if (leftFlightStick.getButton(Button.TWO)) { //Button #2
+            leftPusher.set(-0.7);
+        } else {
+            leftPusher.set(0);
+        }
 
-		} else {
-			climberFront.set(0.0);	
-		}
-	}
+        if (rightFlightStick.getButton(Button.ONE)) {
+            rightPusher.set(0.7);
+        } else if (rightFlightStick.getButton(Button.TWO)) { //Button #2d
+            rightPusher.set(-0.7);
+        } else {
+            rightPusher.set(0);
+        }
 
-	private void moveBackClimber() {
-		//Spin climber in a direction that causes the robot to go down
-		if(controller.getButton(Button.Y)) {
-			climberBack.set(0.5);
-		
-		//Spin climber in direction that causes the robot to go up
-		} else if(controller.getButton(Button.X)) {
-			climberBack.set(-1);
-
-		} else {
-			climberBack.set(0.0);
-		}
-	}
+    }
+    
 }
