305196c53c698f74ddbb584e16a3bb4fc254c04a
JohnTheRobot <Johntherobot5000@gmail.com>
2019-04-11 13:55:07 -0400

    More updates to the climber from testing
    The buttons weren't correct and the id of the climber motor was fixed.

--- src/main/java/org/livoniawarriors/Robot2019/subsystems/gameplay/Climber.java
+++ src/main/java/org/livoniawarriors/Robot2019/subsystems/gameplay/Climber.java
@@ -1,5 +1,6 @@
 package org.livoniawarriors.Robot2019.subsystems.gameplay;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMax;
@@ -16,17 +17,22 @@ import org.livoniawarriors.Robot2019.UserInput.Joystick;
 
 public class Climber {
 
-    private static final int CLIMBER = 23;
-    private static final int LEFT_PUSHER = 17; //TODO: Set to real numbers
+    private static final int CLIMBER = 12;
+    private static final int LEFT_PUSHER = 17;
     private static final int RIGHT_PUSHER = 18;
+    private static final double PUSHER_SPEED = 0.95;
+    private static final double CLIMBER_SPEED = 0.7;
     private CANSparkMax climber;
     private WPI_TalonSRX leftPusher, rightPusher;
 	private UserInput.Controller leftFlightStick, rightFlightStick;
 
 	public Climber() {
         climber = new CANSparkMax(CLIMBER, MotorType.kBrushless);
+        climber.setIdleMode(IdleMode.kBrake);
         leftPusher = new WPI_TalonSRX(LEFT_PUSHER);
         rightPusher = new WPI_TalonSRX(RIGHT_PUSHER);
+        leftPusher.setNeutralMode(NeutralMode.Brake);
+        rightPusher.setNeutralMode(NeutralMode.Brake);
         rightPusher.setInverted(true);
         
 	    leftFlightStick = Robot.userInput.getController(Controllers.L_FLIGHTSTICK);
@@ -38,27 +44,27 @@ public class Climber {
 		if(!enabled) {
             return;
         }
-		
-		if(rightFlightStick.getButton(Button.THREE)) {
-			climber.set(-0.8);
-        } else if (rightFlightStick.getButton(Button.FOUR)) {
-            climber.set(0.8);
+        
+		if(rightFlightStick.getButton(Button.FOUR)) {
+            climber.set(-CLIMBER_SPEED);
+        } else if (rightFlightStick.getButton(Button.FIVE)) {
+            climber.set(CLIMBER_SPEED);
         } else {
             climber.set(0);
         }
 
-        if (leftFlightStick.getButton(Button.ONE)) {
-            leftPusher.set(0.7);
-        } else if (leftFlightStick.getButton(Button.TWO)) { //Button #2
-            leftPusher.set(-0.7);
+        if (leftFlightStick.getButton(Button.THREE)) {
+            leftPusher.set(PUSHER_SPEED);
+        } else if (leftFlightStick.getButton(Button.TWO)) {
+            leftPusher.set(-PUSHER_SPEED);
         } else {
             leftPusher.set(0);
         }
 
-        if (rightFlightStick.getButton(Button.ONE)) {
-            rightPusher.set(0.7);
-        } else if (rightFlightStick.getButton(Button.TWO)) { //Button #2d
-            rightPusher.set(-0.7);
+        if (rightFlightStick.getButton(Button.THREE)) {
+            rightPusher.set(PUSHER_SPEED);
+        } else if (rightFlightStick.getButton(Button.TWO)) {
+            rightPusher.set(-PUSHER_SPEED);
         } else {
             rightPusher.set(0);
         }
